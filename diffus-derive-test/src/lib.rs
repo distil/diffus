03eda58b5509bc4e2d3fa399a60ca82d0c6fbb65
Jim Holmstrom <jim.holmstroem@gmail.com>
2019-09-07 15:42:51 +0000

    created a proper diffus-derive test

---
+++ diffus-derive-test/src/lib.rs
@@ -0,0 +1,70 @@
+use diffus_derive::{
+    Diffus,
+};
+
+use diffus::{
+    Diffable,
+    Edit,
+};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Diffus, Debug)]
+    struct Inner {
+        x: String,
+        y: String,
+    }
+
+    #[derive(Diffus, Debug)]
+    struct Outer {
+        inner: Inner,
+        other: i32,
+    }
+
+    #[test]
+    fn nested() {
+        let left = Outer {
+            inner: Inner {
+                x: "x".to_owned(),
+                y: "y left".to_owned(),
+            },
+            other: 3,
+        };
+        let right = Outer {
+            inner: Inner {
+                x: "x".to_owned(),
+                y: "y right".to_owned(),
+            },
+            other: 3,
+        };
+
+        let diff = left.diff(&right);
+
+        assert_eq!(
+            diff.change().unwrap()
+                .inner.change().unwrap()
+                .y.change().unwrap(),
+            &("y left", "y right")
+        );
+
+        if let Edit::Change(diff) = diff {
+            match (diff.inner, diff.other) {
+                (Edit::Change(diff), Edit::Copy) => {
+                    match (diff.x, diff.y) {
+                        (Edit::Copy, Edit::Change((left, right))) => {
+                            assert_eq!(left, "y left");
+                            assert_eq!(right, "y right");
+                        },
+                        _ => unreachable!("here"),
+                    }
+                },
+                _ => unreachable!("there"),
+            }
+        } else {
+            unreachable!("somewhere")
+        }
+
+    }
+}
