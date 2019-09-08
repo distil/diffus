c99b3e9e224a17c6d90de47dc4985d3d5580d2e6
Jim Holmstrom <jim.holmstroem@gmail.com>
2019-09-08 19:26:16 +0000

    fix changeset + take reference

--- diffus/src/diffable_impls/string.rs
+++ diffus/src/diffable_impls/string.rs
@@ -10,12 +10,17 @@ impl<'a> Diffable<'a> for String {
     fn diff(&'a self, other: &'a Self) -> Edit<'a, Self> {
         use difference;
 
-        let (edit_distance, value_diffs) = difference::ChangeSet::new(self, other, "");
+        let difference::Changeset {
+            diffs: value_diffs,
+            split: _,
+            distance: edit_distance,
+        }  = difference::Changeset::new(self, other, "");
+
         let value_diffs = value_diffs.iter().map(|value_diff| {
             match value_diff {
                 difference::Difference::Same(_) => Edit::Copy,
                 difference::Difference::Rem(_) => Edit::Remove,
-                difference::Difference::Add(a) => Edit::Insert(a),
+                difference::Difference::Add(a) => Edit::Insert(&a),
             }
         })
         .collect::<Vec<_>>();
