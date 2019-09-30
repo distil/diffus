c527e59f13ea34d02ef0512a0cedc7ceadaf5487
Jim Holmstrom <jim.holmstroem@gmail.com>
2019-09-30 23:26:09 +0200

    fix &a&a -> &a&_ for impl Diffable &'a T

--- diffus/src/diffable_impls/mod.rs
+++ diffus/src/diffable_impls/mod.rs
@@ -4,11 +4,12 @@ pub mod string;
 pub mod collection;
 pub mod option;
 
-impl<'a, T: crate::Diffable<'a> + 'a> crate::Diffable<'a> for &'a T {
+impl<'a, T: crate::Diffable<'a> + 'a> crate::Diffable<'a> for &'a T
+{
     type D = T::D;
-    type Target = &'a Self;
+    type Target = T::Target;
 
-    fn diff(&'a self, other: &'a Self) -> crate::edit::Edit<'a, Self::Target> {
+    fn diff(&self, other: &Self) -> crate::edit::Edit<'a, Self::Target> {
         (*self).diff(*other)
     }
 }
